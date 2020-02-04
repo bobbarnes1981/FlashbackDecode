73278358202751f707352513fa0a356c6744261c
Robert Barnes <robertbarnes@googlemail.com>
2020-02-04 06:47:09 +0000

    Add some tests

--- Decoder/Decoder.UnitTests/M68k/OpCodes/ADDDnToByteTests.cs
+++ Decoder/Decoder.UnitTests/M68k/OpCodes/ADDDnToByteTests.cs
@@ -9,19 +9,19 @@ using System.Threading.Tasks;
 namespace Decoder.UnitTests.M68k.OpCodes
 {
     [TestFixture]
-    class ADDByteToDnTests
+    class ADDDnToByteTests
     {
-        [Test(Description = "ADD D1,D0")]
-        public void CheckAddD1ToD0()
+        [Test(Description = "ADD D0,D1")]
+        public void CheckAddD0ToD1()
         {
             // ADD Dn,<ea>
             // 1101 dddD ssmm mxxx
-            // 1101 0010 0000 0000  0xD200
-            // ADD D1,D0
+            // 1101 0001 0000 0001  0xD101
+            // ADD D0,D1
 
             byte[] data = new byte[]
             {
-                0xD2, 0x00
+                0xD1, 0x01
             };
 
             MegadriveState state = new MegadriveState(new Data(data), 0x00000000, 0x00000000, 0x000000, 0x3FFFFF, 0x0FF0000, 0xFFFFFF);
@@ -29,12 +29,12 @@ namespace Decoder.UnitTests.M68k.OpCodes
             state.WriteDReg(0x1, 0x33332222);
             state.FetchOpCode();
 
+            //throw exception
             var opcode = new ADD(state);
 
-            Assert.That(opcode.Assembly, Is.EqualTo("ADD D1,D0"));
+            Assert.That(opcode.Assembly, Is.EqualTo("ADD D0,D1"));
             Assert.That(state.PC, Is.EqualTo(0x02));
-            Assert.That(state.ReadDReg(0x0), Is.EqualTo(0x22223333));
-            Assert.That(state.ReadDReg(0x1), Is.EqualTo(0x33332222));
+            Assert.That(state.ReadDReg(0x1), Is.EqualTo(0x00005678));
         }
 
         [Test(Description = "ADD D0,A1")]
@@ -42,12 +42,12 @@ namespace Decoder.UnitTests.M68k.OpCodes
         {
             // ADD Dn,<ea>
             // 1101 dddD ssmm mxxx
-            // 1101 0000 0000 1001  0xD009
+            // 1101 0001 0000 1001  0xD109
             // ADD D0,D1
 
             byte[] data = new byte[]
             {
-                0xD0, 0x09
+                0xD1, 0x09
             };
 
             MegadriveState state = new MegadriveState(new Data(data), 0x00000000, 0x00000000, 0x000000, 0x3FFFFF, 0x0FF0000, 0xFFFFFF);
@@ -63,17 +63,19 @@ namespace Decoder.UnitTests.M68k.OpCodes
             Assert.That(state.ReadAReg(0x1), Is.EqualTo(0x00005678));
         }
 
-        // Dn
-        // An (not for byte)
+        // to
+        // NOT Dn
+        // NOT An
         // (An)
         // (An)+
         // -(An)
         // (d16, An)
-        // (d8,AN,Xn)
+        // (d8,An,Xn)
         // (xxx).W
-        // (xxx).L
-        // #<data>
-        // (d16,PC)
-        // (d8,PC,Xn)
+        // (xxx}.L
+        // NOT #<data>
+        // NOT (d16,PC)
+        // NOT (d8,PC,Xn)
+
     }
 }
